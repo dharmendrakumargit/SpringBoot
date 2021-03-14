68a1af3af82276c2fba93e7433dcb14b4f05169a
dharmendrakumar <dharmendra.ksonu@gmail.com>
2021-03-14 20:15:25 +0530

    Creating and population data to Table in H2 DataBase

--- jdbc-demo/src/main/java/com/jdbc/example/jdbcdemo/JdbcDemoApplication.java
+++ jdbc-demo/src/main/java/com/jdbc/example/jdbcdemo/JdbcDemoApplication.java
@@ -2,6 +2,15 @@ package com.jdbc.example.jdbcdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+/* Author Dharmendra Kumar 
+   14  Mar 2021
+
+   Creating and populating the data to Table in H2 DataBase
+   Due to Spring Boot Autoconfiguration feature ,Spring boot Automatically read the data.sql File which is in class path.
+   Automatically Table will be created and data will be inserted in Table of H2 DataBase.
+   please go through pom.xml  where we have added H2 Db Starter ,so H2 Db will also be Autoconfigured.
+   web starter added for H2 DB Web Console(H2 DB Web client (ex - similar as tode/sql developer) )
+  */
 
 @SpringBootApplication
 public class JdbcDemoApplication {
